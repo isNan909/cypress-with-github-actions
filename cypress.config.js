d2ce6505216fdbc7d8c4bfba6ec2390cbe8d33e7
ishan-me <ishan02016@gmail.com>
2022-09-16 17:24:18 +0545

    recording key cypress test

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
+    env: {
+      CYPRESS_RECORD_KEY: "dt1t3n"
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
